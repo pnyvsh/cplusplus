f2355ec5d8261f8bedbe2b64306a2032fc8ef34f
punya <pnyvsh@gmail.com>
2018-11-03 09:00:00 +0530

    learnt to join strings from input

---
+++ 00-udemy/10-consoleinputstrings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  string name, surname;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Enter your surname: ";
+  cin >> surname;
+  cout << "Welcome, " << name + " " + surname;
+}
