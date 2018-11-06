4bf3ebe60e0a13e9fbd96de6dbf11a496f2c2222
punya <pnyvsh@gmail.com>
2018-11-06 09:00:00 +0530

    combinedstrings

---
+++ 00-udemy/07-combinedstrings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  string one = "Hi there, ";
+  string two = "this is C++";
+  string combinedStrings = one + two;
+  cout << combinedStrings;
+}
