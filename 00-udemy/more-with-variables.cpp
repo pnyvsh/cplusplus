b8ab96ea2e63b1f8f3dba19dc67e5e217f1bfa4e
punya <pnyvsh@gmail.com>
2018-11-07 09:00:00 +0530

    more with variables

---
+++ 00-udemy/more-with-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a = 0, b = 10, c = 20;
+  cout << "value of a " << a << " and the address: " << &a << "\n";
+  cout << "value of b " << b << " and the address: " << &b << "\n";
+  cout << "value of c " << c << " and the address: " << &c << "\n";
+  short t1 = 5;
+  cout << "value of t1 " << t1 << " and the address: " << &t1 << "\n";
+
+
+}
