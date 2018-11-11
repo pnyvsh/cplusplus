f412d4fca5c54f864d397d067f81e72a0e9ce7f4
punya <pnyvsh@gmail.com>
2018-11-11 09:00:00 +0530

    comments

---
+++ 00-udemy/comments.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main
+{
+  cout << "Learning how to use comments"; //This is how a single-line comment can be used
+}
+
+/* And this is how a multi-line comment
+can be used to be very
+honest */
