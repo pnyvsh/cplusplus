6f44d6c8247be4e9a06178c197d3c337208ee12f
punya <pnyvsh@gmail.com>
2018-11-11 09:00:00 +0530

    learnt about variables/

---
+++ 00-udemy/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  //If we need to print a number out, we can easily print it out using cout << 4; however it won't be stored permanantly
+  //Hence, we can store the digits in a variable
+  int A;
+  //Here, we've initialised a variable we can store a value in
+  A = 4;
+  cout << A;
+  //How, the output should be "4," and we now also have it stored.
+}
